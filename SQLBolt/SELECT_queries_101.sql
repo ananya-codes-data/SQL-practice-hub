0904d617e0eeb65d314be8975198265be78346f2
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-12 19:18:46 +0530

    add: SQL solutions for SQLBolt problems

---
+++ SQLBolt/SELECT_queries_101.sql
@@ -0,0 +1,31 @@
+-- Platform: SQLBolt
+-- Problem: https://sqlbolt.com/lesson/select_queries_introduction
+-- Concepts: SELECT
+
+-- Solution:
+-- 1.
+SELECT
+    title
+FROM movies;
+
+-- 2.
+SELECT
+    director
+FROM movies;
+
+-- 3.
+SELECT
+    title,
+    director
+FROM movies;
+
+-- 4.
+SELECT
+    title,
+    year
+FROM movies;
+
+-- 5.
+SELECT
+*
+FROM movies;
