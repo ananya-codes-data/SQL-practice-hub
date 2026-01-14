3dd2a852cae1eb1706f6e3e9bf2f78fff2e6d1ec
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-14 19:23:33 +0530

    add: SQL solutions for HackerRank problems

---
+++ HackerRank/Weather_Observation_Station_1.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Problem:https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+
+-- Solution:
+SELECT
+    city,
+    state
+FROM station;
