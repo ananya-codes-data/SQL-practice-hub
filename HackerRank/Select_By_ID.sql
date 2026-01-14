e5f858e1e755b3cb9c5ba24e34988f58f1a6e092
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-14 19:13:19 +0530

    add: SQL solutions for HackerRank problems

---
+++ HackerRank/Select_By_ID.sql
@@ -0,0 +1,10 @@
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Problem:https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+
+-- Solution:
+SELECT
+*
+FROM city
+WHERE
+    ID = 1661;
