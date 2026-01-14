1390293a0c6ce44039872a0bc9321d175edc58bf
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-14 19:16:32 +0530

    add: SQL solutions for HackerRank problems

---
+++ HackerRank/Japanese_Cities_Attributes.sql
@@ -0,0 +1,10 @@
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Problem:https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+
+-- Solution:
+SELECT
+*
+FROM city
+WHERE
+    countrycode = 'JPN';
