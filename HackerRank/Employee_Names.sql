17c1f9f6582ca4c70a3dd5a065b63d6c5068538b
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-15 23:28:58 +0530

    add: SQL solutions for HackerRank problems

---
+++ HackerRank/Employee_Names.sql
@@ -0,0 +1,10 @@
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Problem:https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+
+-- Solution:
+SELECT
+    name
+FROM employee
+ORDER BY
+    name ASC;
