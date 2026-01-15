9e91c1d13b812d90129144238944bb402a008ff2
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-15 22:59:35 +0530

    add: SQL solutions for HackerRank problems

---
+++ HackerRank/Weather_Observation_Station_8.sql
@@ -0,0 +1,24 @@
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Problem:https://www.hackerrank.com/challenges/weather-observation-station-8/problem?isFullScreen=true
+
+-- Solution:
+SELECT DISTINCT
+    city
+FROM station
+WHERE
+    (city LIKE 'A%' OR
+    city LIKE 'E%' OR
+    city LIKE 'I%' OR
+    city LIKE 'O%' OR
+    city LIKE 'U%')
+    AND
+    (city LIKE '%a'
+    OR
+    city LIKE '%e'
+    OR
+    city LIKE '%i'
+    OR
+    city LIKE '%o'
+    OR
+    city LIKE '%u');
