8e23ed0e197c4e08364845f93fe581cd71e51ba0
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-15 23:16:57 +0530

    add: SQL solutions for HackerRank problems

--- HackerRank/Weather_Observation_Station_10.sql
+++ HackerRank/Weather_Observation_Station_10.sql
@@ -3,3 +3,16 @@
 -- Problem:https://www.hackerrank.com/challenges/weather-observation-station-10/problem?isFullScreen=true
 
 -- Solution:
+SELECT DISTINCT
+    city
+FROM station
+WHERE  
+    city NOT LIKE '%a'
+    AND
+    city NOT LIKE '%e'
+    AND
+    city NOT LIKE '%i'
+    AND
+    city NOT LIKE '%o'
+    AND
+    city NOT LIKE '%u';
