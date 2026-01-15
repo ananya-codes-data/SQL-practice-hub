1fa48b912184eea0a481ece470c482f1f6371465
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-15 23:29:31 +0530

    fix: file name

---
+++ HackerRank/Higher_Than_75_Marks.sql
@@ -0,0 +1,13 @@
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Problem:https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+
+-- Solution:
+SELECT
+    name
+FROM students
+WHERE
+    marks > 75
+ORDER BY
+    RIGHT(name, 3) ASC,
+    id ASC;
