b0744920bc51127c5d006d4901c9896fd65e7318
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-08-21 23:57:03 +0530

    add: SQL solutions for Leetcode problems

---
+++ Leetcode/Exchange-seats.sql
@@ -0,0 +1,14 @@
+-- Platform: Leetcode
+-- Difficulty: Easy
+-- Problem: https://leetcode.com/problems/exchange-seats/description/
+
+-- Solution:
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM seat) THEN id
+        WHEN id % 2 = 0 THEN id - 1
+        WHEN id % 2 = 1 THEN id + 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id ASC;
