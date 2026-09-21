26cde2b333f700634538fadbf919b0d1551b8198
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-09-21 14:40:44 +0530

    add: SQL solutions for DataLemur problems

---
+++ DataLemur/Data_Science_Skills.sql
@@ -0,0 +1,12 @@
+-- Platform: DataLemur
+-- Difficulty: Easy
+-- Problem: https://datalemur.com/questions/matching-skills
+
+-- Solution:
+SELECT
+  candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(candidate_id) = 3
+ORDER BY candidate_id;
