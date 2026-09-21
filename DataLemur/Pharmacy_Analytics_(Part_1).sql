3773688b3da9c300cf0edb39965aca6b7879b84e
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-09-21 14:24:09 +0530

    add: SQL solutions for DataLemur problems

---
+++ DataLemur/Pharmacy_Analytics_(Part_1).sql
@@ -0,0 +1,12 @@
+-- Platform: DataLemur
+-- Difficulty: Easy
+-- Problem: https://datalemur.com/questions/top-profitable-drugs
+
+-- Solution:
+SELECT
+  drug,
+  SUM(total_sales) - SUM(cogs) AS total_profit
+FROM pharmacy_sales
+GROUP BY drug
+ORDER BY total_profit DESC
+LIMIT 3;
