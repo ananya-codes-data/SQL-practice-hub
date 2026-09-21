c1361e2a55646e32c73dd7d4feae30e4519d5980
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-09-21 14:40:02 +0530

    add: SQL solutions for DataLemur problems

---
+++ DataLemur/Laptop_vs_Mobile_Viewership.sql
@@ -0,0 +1,9 @@
+-- Platform: DataLemur
+-- Difficulty: Easy
+-- Problem: https://datalemur.com/questions/laptop-mobile-viewership
+
+-- Solution:
+SELECT
+  SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+  SUM(CASE WHEN device_type IN ('phone', 'tablet') THEN 1 ELSE 0 END) AS mobile_views
+FROM viewership;
